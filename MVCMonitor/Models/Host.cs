9bcbe75de53f30111b647b0e0bdfe22b156c9d29
OllegM <aarc@live.ru>
2016-09-20 02:12:25 +0300

    Commit #2

--- MVCMonitor/Models/Host.cs
+++ MVCMonitor/Models/Host.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,12 @@ namespace MVCMonitor.Models
 {
     public class Host
     {
+        public int HostId { get; set; }
+        [Required()]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
+        [Required()]
+        [DataType(DataType.Text)]
         public string IPAddress { get; set; }
     }
 }
