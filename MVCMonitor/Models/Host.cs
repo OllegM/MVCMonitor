b8567f06e582485904f3dc39d2185798e878673f
OllegM <aarc@live.ru>
2016-09-20 01:53:10 +0300

    New model class

---
+++ MVCMonitor/Models/Host.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMonitor.Models
+{
+    public class Host
+    {
+        public string Name { get; set; }
+        public string IPAddress { get; set; }
+    }
+}
