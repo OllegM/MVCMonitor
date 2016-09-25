b5de43fe8d048c2abaf3b7e419b2f05c1d3f921e
OllegM <aarc@live.ru>
2016-09-25 20:27:49 +0300

    New model class

---
+++ MVCMonitor/Scripts/ts/class.js
@@ -0,0 +1,12 @@
+var Host = (function () {
+    function Host() {
+    }
+    Host.prototype.GetHostName = function () {
+        return this.HostName;
+    };
+    Host.prototype.GetIPAddress = function () {
+        return this.HostIPAddress;
+    };
+    return Host;
+}());
+//# sourceMappingURL=class.js.map
---
+++ MVCMonitor/Scripts/ts/class.ts
@@ -0,0 +1,12 @@
+﻿class Host {
+    public HostName: string;
+    public HostIPAddress: string;
+
+    public GetHostName():string {
+        return this.HostName;
+    }
+
+    public GetIPAddress():string {
+        return this.HostIPAddress;
+    }
+}
